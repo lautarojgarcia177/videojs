993e60127eb93596771ed590c3f7c18b4cd9ab08
lautarojgarcia177 <lautarojgarcia177@gmail.com>
2020-06-12 15:11:45 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+    <app-player></app-player>    
+  `,
+  styles: []
+})
+export class AppComponent {
+}
---
+++ src/app/components/player/player.component.ts
@@ -0,0 +1,111 @@
+import { Component, OnInit, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
+import { interval } from 'rxjs';
+import videojs from 'video.js';
+
+@Component({
+  selector: 'app-player',
+  templateUrl: './player.component.html',
+  styleUrls: [
+    './player.component.scss'
+  ],
+  encapsulation: ViewEncapsulation.None,
+})
+export class PlayerComponent implements OnInit {
+
+  // videoUrl = '//vjs.zencdn.net/v/oceans.mp4';
+  videoUrl = '//archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4';
+
+  seekTime = 0;
+
+  @ViewChild('target', {static: true}) target: ElementRef;
+
+  player: videojs.Player;
+
+  options = {
+  autoplay: true,
+  controls: true,
+  sources: [{ src: this.videoUrl, type: 'video/mp4' }],
+  controlBar: {
+    'pictureInPictureToggle': false
+  },
+  aspectRatio: '16:9',
+  fluid: true,
+  };
+
+  notifyCurrentTime$ = interval(5000);
+  currentPlayingTime = 0;
+  notifyCurrentTime = this.notifyCurrentTime$.subscribe( () => {
+    this.currentPlayingTime = this.target.nativeElement.currentTime;
+  });
+
+  lastPlayingTime$ = interval(100);
+  lastPlayingTime = 0;
+  _nextTimeStartAt; 
+ 
+  volume;
+
+  ended = false;
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit(): void {
+        // instantiate Video.js
+        this.player = videojs(this.target.nativeElement, this.options, () => {
+          this.volume = this.player.volume();
+
+          if (localStorage.getItem('videoUrl')) {
+            this.videoUrl = localStorage.getItem('videoUrl');
+            this.player.src(this.videoUrl);
+          } 
+          localStorage.setItem('videoUrl', this.videoUrl);
+
+          this.player.on('ended', () => this.onEnd());
+          this.player.on('volumechange', () => this.onVolumeChange());
+          this.player.on('play', () => this.onPlay());
+
+          this.player.play();
+
+          console.log('videoUrl', this.videoUrl);
+        });
+  }
+
+  onEnd() {
+    this.ended = true;
+  }
+
+  onVolumeChange() {
+    this.volume = this.player.volume();
+  }
+
+  onPlay() {
+    if (localStorage.getItem('videoLatestCurrentTime')) {
+      this.target.nativeElement.currentTime = localStorage.getItem('videoLatestCurrentTime');
+    }
+    this._nextTimeStartAt = this.lastPlayingTime$.subscribe( () => this.nextTimeStartAt(this.target.nativeElement.currentTime));
+    this.ended = false;
+  }
+
+  ngOnDestroy() {
+    // destroy player
+    if (this.player) {
+      this.player.dispose();
+    }
+    this.nextTimeStartAt(this.target.nativeElement.currentTime);
+  }
+
+  watch() {
+    this.player.src(this.videoUrl);
+    this.ended = false;
+    localStorage.setItem('videoUrl', this.videoUrl);
+  }
+
+  nextTimeStartAt(time) {
+    localStorage.setItem('videoLatestCurrentTime', time);
+  }
+
+  seek() {
+    this.target.nativeElement.currentTime = this.seekTime;
+    this.player.play();
+  }
+
+}
